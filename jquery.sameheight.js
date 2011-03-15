5967940b83c14f5047863f1a6529c40f20af676b
Justin Force <justin.force@gmail.com>
2011-03-15 09:33:21 -0700

    Add header and don't use $ for jQuery

--- jquery.sameheight.js
+++ jquery.sameheight.js
@@ -1,14 +1,20 @@
-(function($) {
-  $.fn.sameHeight = function() {
-    var these = this;
-    function setHeight() {
-      var max = 0;
-      these.height('auto').each(function() {
-        max = Math.max(max, $(this).height());
-      }).height(max);
-    };
-    $(window).resize(setHeight);
-    setHeight();
-    return this;
+/*
+* sameHeight jQuery plugin
+* http://github.com/sidewaysmilk/jquery-sameheight
+*
+* Copyright 2011, Justin Force
+* Licensed under the MIT license
+*/
+jQuery.fn.sameHeight = function() {
+  var these = this;
+  function setHeight() {
+    var max = 0;
+    these.height('auto').each(function() {
+      max = Math.max(max, jQuery(this).height());
+    }).height(max);
   };
-})(jQuery);
+  jQuery(window).resize(setHeight);
+  setHeight();
+  return this;
+};
+
