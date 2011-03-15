d0b3ca522959b6606db9f49dba06d1db817cd1ef
Justin Force <justin.force@gmail.com>
2011-03-15 11:00:49 -0700

    Add $.sameHeight

--- jquery.sameheight.js
+++ jquery.sameheight.js
@@ -5,6 +5,10 @@
 * Copyright 2011, Justin Force
 * Licensed under the MIT license
 */
+
+/*
+* for $('.someClass').sameHeight()
+*/
 jQuery.fn.sameHeight = function() {
   var these = this;
   function setHeight() {
@@ -16,5 +20,28 @@ jQuery.fn.sameHeight = function() {
   jQuery(window).resize(setHeight);
   setHeight();
   return this;
+};
+
+/*
+* for $.sameHeight()
+* Applies to everything with class="sameheight sh_something"
+*/
+jQuery.sameHeight = function() {
+  jQuery('.sameheight').each(function() {
+    jThis = jQuery(this);
+    var selectors = [];
+
+    // group by class="sh_whatever"
+    var match = jThis.attr('class').match(/(sh_\S+)/);
+    if (match && match[1]) {
+      if (selectors.indexOf(match[1]) < 0) {
+        selectors.push(match[1]);
+      }
+    }
+
+    jQuery.each(selectors, function(index, value) {
+      jQuery('.' + value).sameHeight();
+    });
+  });
 };
 
